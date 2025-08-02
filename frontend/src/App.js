1c1548a62e53daf01f610da056e52b6b91dc4f0c
Kamleshb231 <158018416+Kamleshb231@users.noreply.github.com>
2025-08-02 19:34:03 +0530

    correcting structure

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Event Management</h1>
+      </header>
+      <main>
+        {/* Add your components here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
