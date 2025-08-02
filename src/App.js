d2f94392249656c0770a118dfa48498e594680d7
Kamleshb231 <158018416+Kamleshb231@users.noreply.github.com>
2025-08-02 19:20:32 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Event Management</h1>
+      {/* Your app content will go here */}
+    </div>
+  );
+}
+
+export default App;
